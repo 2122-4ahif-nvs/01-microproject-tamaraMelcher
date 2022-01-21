381318d0c9d136b746b7ef8e56eacf199fc6737a
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2022-01-21 18:24:21 +0100

    added some data in import.sql file

--- src/main/resources/imports.sql
+++ src/main/resources/imports.sql
@@ -1,10 +1,13 @@
 insert into mp_league (l_difficulty, l_name_of_league)
-values (1, 'League NR.3'),
+values (1, 'League NR.1'),
        (2, 'League NR.2'),
-       (3, 'League NR.1');
+       (3, 'League NR.3');
 
 insert into MP_CLIMBER (c_first_name, c_last_name, c_age, LEAGUE_L_ID)
-values ('Tamara', 'Melcher', 18, 3),
-       ('Jakob', 'Unterberger', 21, 1),
-       ('Julia', 'Koenig', 17, 1),
-       ('Jan', 'Melcher', 16, 2);
+values ('Tamara', 'Melcher', 18, 1),
+       ('Jakob', 'Unterberger', 21, 2),
+       ('Julia', 'Koenig', 17, null),
+       ('Alina', 'Schuster', 17, 2),
+       ('Anna', 'Wiesinger', 17, 1),
+       ('Elena', 'Heckmann', 17, 3),
+       ('Jan', 'Melcher', 16, 3);
