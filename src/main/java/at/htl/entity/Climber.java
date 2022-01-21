6b1205782d4cce570e085925ff4e5c6c7383d157
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2022-01-21 19:36:06 +0100

    added jpa security

--- src/main/java/at/htl/entity/Climber.java
+++ src/main/java/at/htl/entity/Climber.java
@@ -2,25 +2,18 @@ package at.htl.entity;
 import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
-
+import io.quarkus.security.jpa.Password;
+import io.quarkus.security.jpa.Roles;
+import io.quarkus.security.jpa.UserDefinition;
+import io.quarkus.security.jpa.Username;
 import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
-@NamedQueries({
-        @NamedQuery(
-                name = "Climber.getAllClimbers",
-                query = "select c from Climber c"
-        ),
-        @NamedQuery(
-                name = "Climber.getClimberPerId",
-                query = "select c from Climber c where c.id = :ID"
-        )
-})
-
 @Table(name = "MP_CLIMBER")
 @Entity
+@UserDefinition
 public class Climber extends PanacheEntityBase {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "C_ID")
@@ -36,8 +29,15 @@ public class Climber extends PanacheEntityBase {
     @Max(message="Climber has to be younger than 100 years", value=100)
     @Column(name = "C_AGE")
     public int age;
+    @Username
     @Column(name = "C_USERNAME")
-    public String userName;
+    public String username;
+    @Password
+    @Column(name = "C_PASSWORD")
+    public String password;
+    @Roles
+    @Column(name = "C_ROLE")
+    public String role;
     @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST})
     public League league;
 
@@ -49,11 +49,22 @@ public class Climber extends PanacheEntityBase {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
-        this.userName = firstName.toLowerCase() + lastName;
     }
 
     public Climber(String firstName, String lastName, int age, League league) {
-    this();
+        this();
+        this.league = league;
+    }
+
+    public Climber(String firstName, String lastName, int age, String username, String password, String role) {
+        this();
+        this.username = username;
+        this.password = BcryptUtil.bcryptHash(password);
+        this.role = role;
+    }
+
+    public Climber(String firstName, String lastName, int age, League league, String username, String password, String role) {
+        this();
         this.league = league;
     }
     //endregion
--- src/main/resources/imports.sql
+++ src/main/resources/imports.sql
@@ -11,3 +11,12 @@ values ('Tamara', 'Melcher', 18, 1),
        ('Anna', 'Wiesinger', 17, 1),
        ('Elena', 'Heckmann', 17, 3),
        ('Jan', 'Melcher', 16, 3);
+
+insert into MP_CLIMBER (c_first_name, c_last_name, c_age, LEAGUE_L_ID, c_username, c_password, c_role)
+values ('Tamara', 'Melcher', 18, 1,'tamaraMelcher', '1234', 'climber'),
+       ('Jakob', 'Unterberger', 21, 2, 'jakobUnterberger', '4321', 'climber'),
+       ('Julia', 'Koenig', 17, null, 'juliaKoenig', 'hello', 'trainer'),
+       ('Alina', 'Schuster', 17, 2, 'alinaSchuster', 'hi', 'climber'),
+       ('Anna', 'Wiesinger', 17, 1, 'annaWiesinger', 'juli', 'climber'),
+       ('Elena', 'Heckmann', 17, 3, 'elenaHeckmann', 'einhorn', 'climber'),
+       ('Jan', 'Melcher', 16, 3, 'janMelcher', 'pokemon', 'trainer');
