35e5e8efba5461d0308c208734bdea69959ba7d0
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2022-02-10 21:09:29 +0100

    some security

--- src/main/java/at/htl/boundary/ClimberAPI.java
+++ src/main/java/at/htl/boundary/ClimberAPI.java
@@ -1,18 +1,33 @@
 package at.htl.boundary;
 
+import at.htl.control.ClimberRepository;
+import io.quarkus.security.identity.SecurityIdentity;
+
 import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
 
+@Path("climber")
 public class ClimberAPI {
+    @Inject
+    ClimberRepository climberRepository;
+
+    @Inject
+    SecurityIdentity securityIdentity;
 
     @GET
-    @RolesAllowed("customer")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String customer(@Context SecurityContext securityContext) {
-        return securityContext.getUserPrincipal().getName();
+    @Path("getAll")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllClimbers() {
+        if(!securityIdentity.hasRole("climber") && !securityIdentity.hasRole("admin")) {
+            return Response.status(403).build();
+        }
+        return Response.ok(climberRepository.findAll().list()).build();
     }
 }
