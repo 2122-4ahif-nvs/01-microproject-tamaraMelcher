c13fa6bd79c0a4f70fbb4cf58feb8287b7a3446b
tamaraMelcher <t.melcher.htlleonding@gmail.com>
2022-02-10 17:31:42 +0100

    added graphql

---
+++ src/main/java/at/htl/boundary/ClimberGraphQLResource.java
@@ -0,0 +1,29 @@
+package at.htl.boundary;
+
+import at.htl.control.ClimberService;
+import at.htl.entity.Climber;
+import org.eclipse.microprofile.graphql.Description;
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Name;
+import org.eclipse.microprofile.graphql.Query;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@GraphQLApi
+public class ClimberGraphQLResource {
+    @Inject
+    ClimberService climberService;
+
+    @Query("getAllClimbers")
+    @Description("Get all climbers")
+    public List<Climber> getAllClimbers(){
+        return climberService.getAllClimbers();
+    }
+
+    @Query("getClimberById")
+    @Description("Get a Tenniscourt")
+    public Climber getTenniscourtByCourtId(@Name("climberId") Long id) {
+        return climberService.getClimberById(id);
+    }
+}
